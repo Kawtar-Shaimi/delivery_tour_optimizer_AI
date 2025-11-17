b1053bc24fe1e519e382d85cdf96f72286ede42b
Kawtar <Kawtar.shaimi8@gmail.com>
2025-11-17 17:19:51 +0100

    DTO-36 : Ajout de model controller

---
+++ src/main/java/com/dto/delivery_tour_optimizer/model/Customer.java
@@ -0,0 +1,50 @@
+package com.dto.delivery_tour_optimizer.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "customers")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(max = 255)
+    private String name;
+
+    @Size(max = 512)
+    private String address;
+    private double latitude;
+    private double longitude;
+    @Size(max = 50)
+    private String preferredTimeSlot;
+
+    @Builder.Default
+    @JsonIgnore
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
+    private List<Delivery> deliveries = new ArrayList<>();
+}
